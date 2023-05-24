1d9e27f5ff857ac2bea6e8e10db753cd7ce3a5fa
Sincerely Brittany <sincerelybr@gmail.com>
2023-05-24 15:32:26 -0400

    adds @chakra-ui/react and a container to the App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Data from "./data/Mobile_Food_Facility_Permit.csv";
 import Papa from "papaparse";
+import { Container } from "@chakra-ui/react";
 
 function App() {
   const [isDataLoading, setDataLoading] = React.useState(false);
@@ -27,30 +28,32 @@ function App() {
 
   console.log(data);
   return (
-    <div className="App">
-      {isDataLoading ? (
-        <h1> loading </h1>
-      ) : (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Applicant</th>
-              <th>FacilityType</th>
-              <th>LocationDescription</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, index) => (
-              <tr key={index}>
-                <td>{row.Applicant}</td>
-                <td>{row.FacilityType}</td>
-                <td>{row.LocationDescription}</td>
+    <Container maxW="2xl" bg="blue.600" centerContent>
+      <div className="App">
+        {isDataLoading ? (
+          <h1> loading </h1>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Applicant</th>
+                <th>FacilityType</th>
+                <th>LocationDescription</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {data.map((row, index) => (
+                <tr key={index}>
+                  <td>{row.Applicant}</td>
+                  <td>{row.FacilityType}</td>
+                  <td>{row.LocationDescription}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </Container>
   );
 }
 
