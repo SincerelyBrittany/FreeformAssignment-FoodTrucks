b18e2dccbd5ac25f74b7066fe2605745ad552e71
Sincerely Brittany <sincerelybr@gmail.com>
2023-05-24 15:29:03 -0400

    renders csv file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Data from "./data/Mobile_Food_Facility_Permit.csv";
+import Papa from "papaparse";
 
 function App() {
+  const [isDataLoading, setDataLoading] = React.useState(false);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(Data);
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder("utf-8");
+    const csvData = decoder.decode(result.value);
+    const parsedData = Papa.parse(csvData, {
+      header: true,
+      skipEmptyLines: true,
+    }).data;
+    setData(parsedData || []);
+    setDataLoading(false);
+  };
+
+  useEffect(() => {
+    setDataLoading(true);
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isDataLoading ? (
+        <h1> loading </h1>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Applicant</th>
+              <th>FacilityType</th>
+              <th>LocationDescription</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.Applicant}</td>
+                <td>{row.FacilityType}</td>
+                <td>{row.LocationDescription}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
